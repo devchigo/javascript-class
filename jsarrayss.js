851c1f60ad5ff27c7b43e92163d41530e5569bad
chigozirim1999 <ndinechilex@gmail.com>
2022-12-09 23:58:42 +0100

    js

---
+++ jsarrayss.js
@@ -0,0 +1,13 @@
+function tipCalculator(bill) {
+    let percentage;
+    if (bill < 50) {
+        percentage = 0.2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = 0.15;
+    } else {
+        percentage = 0.1;
+    }
+    return percentage * bill;
+}
+
+console.log(tipCalculator(100));
